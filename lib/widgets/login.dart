ba01b592f75ecb9925d9b76ae20a5f0ac8c2dbf6
gdar463 <gdar463@gmail.com>
2024-03-22 00:44:34 +0100

    Moved app.dart to widgets/login.dart

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -1,26 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:my_classeviva/cvv/auth.dart';
-import 'package:my_classeviva/cvv/exceptions/http_request_exception.dart';
-import 'package:my_classeviva/cvv/exceptions/wrong_credentials_exception.dart';
-import 'package:my_classeviva/cvv/models/auth_response_model.dart';
-import 'package:my_classeviva/cvv/models/profile.dart';
+import "package:flutter/material.dart";
+import "package:my_classeviva/cvv/auth.dart";
+import "package:my_classeviva/cvv/exceptions/http_request_exception.dart";
+import "package:my_classeviva/cvv/exceptions/wrong_credentials_exception.dart";
+import "package:my_classeviva/cvv/models/auth_response_model.dart";
+import "package:my_classeviva/cvv/models/profile.dart";
+import "package:my_classeviva/widgets/main.dart";
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   final String title = "THE MAGICAL WORLD OF ZAMPA";
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class FormData {
@@ -30,11 +22,12 @@ class FormData {
   FormData({this.email, this.password});
 }
 
-class _HomePageState extends State<HomePage> {
+class _LoginPageState extends State<LoginPage> {
   Profile? profile;
   FormData formData = FormData();
 
   bool loading = false;
+  bool loggedIn = true;
 
   void onPressed() async {
     loading = true;
@@ -54,8 +47,6 @@ class _HomePageState extends State<HomePage> {
 
     profile = Profile.fromAuth(auth);
 
-    print(auth);
-
     var firstName = profile?.firstName;
     firstName ??= "null";
 
@@ -66,45 +57,24 @@ class _HomePageState extends State<HomePage> {
     ident ??= "ident";
 
     loading = false;
-    _showDialog(
-        "Logged In!\nNome: $firstName\nCognome: $lastName\nident: $ident");
+    loggedIn = true;
+
+    if (!context.mounted) return;
+
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => MainPage(profile: profile)));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Form(
@@ -168,7 +138,7 @@ class _HomePageState extends State<HomePage> {
         title: Text(message),
         actions: [
           TextButton(
-            child: const Text('OK'),
+            child: const Text("OK"),
             onPressed: () => Navigator.of(context).pop(),
           ),
         ],
