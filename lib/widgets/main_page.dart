dfa160f8052cff4013bdb7ab9d71e1bfe076e962
gdar463 <gdar463@gmail.com>
2024-03-22 22:31:25 +0100

    Reworked main page to include tabs and appbar

--- lib/widgets/main_page.dart
+++ lib/widgets/main_page.dart
@@ -6,13 +6,43 @@ class MainPage extends StatelessWidget {
 
   final Profile? profile;
 
+  ImageProvider<Object> getThemeIcon(BuildContext context) {
+    String theme = "light";
+    if (Theme.of(context).brightness == Brightness.dark) {
+      theme = "dark";
+    }
+    return AssetImage("assets/images/classemista-$theme.png");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title:
-                Text("Buonasera, ${profile?.firstName} ${profile?.lastName}")),
-        body: const Center(child: Text("magic")));
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              children: [
+                const Icon(Icons.account_circle),
+                const SizedBox(width: 10),
+                Image(
+                  image: getThemeIcon(context),
+                  width: MediaQuery.of(context).size.width / 100 * 27.5,
+                ),
+              ],
+            ),
+          ),
+          body: const TabBarView(children: [
+            Center(child: Text("today")),
+            Center(child: Text("book")),
+            Center(child: Text("camera")),
+            Center(child: Text("settings"))
+          ]),
+          bottomNavigationBar: const TabBar(tabs: [
+            Tab(icon: Icon(Icons.today), text: "Today"),
+            Tab(icon: Icon(Icons.book_rounded), text: "Journal"),
+            Tab(icon: Icon(Icons.camera), text: "Camera"),
+            Tab(icon: Icon(Icons.settings), text: "Settings")
+          ]),
+        ));
   }
 }
