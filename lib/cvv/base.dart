392cdadc31270008fb0ccfc0aa216c91cb222e27
gdar463 <gdar463@gmail.com>
2024-03-24 21:30:50 +0100

    Added license header

--- lib/cvv/base.dart
+++ lib/cvv/base.dart
@@ -1,3 +1,22 @@
+//
+// base.dart
+//
+// Copyright (C) 2024 gdar463 <gdar463@gmail.com>
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General
+// Public License along with this program. If not, see
+// <https://www.gnu.org/licenses/>.
+//
+
 class Base {
   // Base API URL
   static const String baseUrl = "https://web.spaggiari.eu/rest/v1";
--- lib/cvv/exceptions/wrong_credentials_exception.dart
+++ lib/cvv/exceptions/wrong_credentials_exception.dart
@@ -1,3 +1,22 @@
+//
+// wrong_credentials_exception.dart
+//
+// Copyright (C) 2024 gdar463 <gdar463@gmail.com>
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General
+// Public License along with this program. If not, see
+// <https://www.gnu.org/licenses/>.
+//
+
 class WrongCredentialsExcpetion implements Exception {
   final String? uid;
   final String? pass;
