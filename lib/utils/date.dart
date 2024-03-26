b915e45cdab1170a8aa83983131f08be346c4cee
gdar463 <gdar463@gmail.com>
2024-03-26 22:02:25 +0100

    Abbreviated all months names in Date

--- lib/utils/date.dart
+++ lib/utils/date.dart
@@ -27,30 +27,30 @@ class Date {
   static String monthToString(int month) {
     switch (month) {
       case 1:
-        return "January";
+        return "Jan";
       case 2:
-        return "Febraury";
+        return "Feb";
       case 3:
-        return "March";
+        return "Mar";
       case 4:
-        return "April";
+        return "Apr";
       case 5:
         return "May";
       case 6:
-        return "June";
+        return "Jun";
       case 7:
-        return "July";
+        return "Jul";
       case 8:
-        return "August";
+        return "Aug";
       case 9:
-        return "September";
+        return "Sep";
       case 10:
-        return "October";
+        return "Oct";
       case 11:
-        return "November";
+        return "Nov";
       case 12:
-        return "December";
+        return "Dec";
     }
-    return "January";
+    return "Jan";
   }
 }
