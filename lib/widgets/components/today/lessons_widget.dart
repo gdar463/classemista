9eddf61713a660d02d411c58e5db2973432ff0f2
gdar463 <gdar463@gmail.com>
2024-03-28 18:20:25 +0100

    Reworked LessonsWidget

--- lib/widgets/components/today/lessons_widget.dart
+++ lib/widgets/components/today/lessons_widget.dart
@@ -51,13 +51,108 @@ class _LessonsWidgetState extends State<LessonsWidget> {
       ) {
         if (snapshot.connectionState == ConnectionState.done &&
             !snapshot.hasError) {
-          return Column(
-            children: [
-              ...snapshot.data!.lessons.map((e) {
-                return Text(e.lessonDescription);
-              }),
-            ],
-          );
+          List<Lesson> lessons = snapshot.data!.lessons;
+          if (lessons.isNotEmpty) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 15),
+                const Row(
+                  children: [
+                    SizedBox(width: 15),
+                    Text(
+                      "Lezioni",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    )
+                  ],
+                ),
+                ...lessons.map((value) {
+                  return Container(
+                    margin: const EdgeInsets.only(
+                      top: 6,
+                      bottom: 6,
+                      left: 8,
+                      right: 8,
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    child: Card(
+                      color: const Color(0xff282828),
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                          width: 4,
+                          strokeAlign: BorderSide.strokeAlignOutside,
+                          color: Color(0x00ffffff),
+                        ),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              value.subject,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(height: 5.25),
+                            Row(
+                              children: [
+                                const Icon(Icons.person, size: 15),
+                                const SizedBox(width: 5),
+                                Text(
+                                  value.author,
+                                  style: const TextStyle(fontSize: 11.5),
+                                ),
+                                const SizedBox(width: 7.5),
+                                const Icon(Icons.schedule, size: 15),
+                                const SizedBox(width: 5),
+                                Text(
+                                  "${value.hour}° ora",
+                                  style: const TextStyle(fontSize: 11.5),
+                                )
+                              ],
+                            ),
+                            Builder(
+                              builder: (BuildContext context) {
+                                if (value.lessonDescription.isNotEmpty) {
+                                  return Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const SizedBox(height: 3.5),
+                                      const Divider(),
+                                      const SizedBox(height: 3.5),
+                                      Text(
+                                        value.lessonDescription,
+                                        style: const TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      )
+                                    ],
+                                  );
+                                }
+                                return const SizedBox();
+                              },
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+                const SizedBox(height: 10),
+              ],
+            );
+          }
+          return const SizedBox();
         } else if (snapshot.hasError) {
           return Text("Error: ${snapshot.error}");
         } else {
